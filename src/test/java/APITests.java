746105004fd6b880ef5c0c459f5aa5e702476958
Carina Leal <carina.leal@trustly.com>
2024-04-25 17:57:50 -0300

    project dependencies and test Examples

---
+++ src/test/java/APITests.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APITests {
+
+    @Test
+    void testExample(){
+        //reqres.in FakeAPI
+
+        Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+        response.getStatusCode();
+        response.getBody();
+        System.out.println("Status Code: "+response.getStatusCode());
+        System.out.println("Response: "+response.asString());
+        System.out.println("Body: "+response.getBody());
+        System.out.println("Time Taken: "+response.getTime());
+        System.out.println("Header: "+response.getHeader("content-type"));
+
+        int statusCode = response.statusCode();
+        Assert.assertEquals(statusCode, 200);
+    }
+}
