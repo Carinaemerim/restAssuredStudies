3de4103532592daf6f9b42426d2cf67fc33ac4df
Carina Leal <carina.leal@trustly.com>
2024-04-30 19:40:13 -0300

    Adding more test cases related to get user by ID

--- src/test/java/serveRestStudy/features/getUsers.feature
+++ src/test/java/serveRestStudy/features/getUsers.feature
@@ -10,6 +10,22 @@ Feature: Get all users from API
     When the get users endpoint is called
     Then the quantity should be retrieved
 
+  Scenario: Verify the get users api first email result
+    Given the base URL is passed
+    When the get users endpoint is called
+    Then the userdata name should be displayed on the result list
+    And the userdata email should be displayed on the result list
+    And the userdata password should be displayed on the result list
+    And the userdata administrator should be displayed on the result list
+    And the userdata _id should be displayed on the result list
 
+  Scenario Outline: Verify the get users api can search for a user ID
+    Given the base URL is passed
+    When a userID <userID> is passed on get users by ID endpoint
+    Then the user <user> should be retrieved
 
+  Examples:
+    | userID             | user              |
+    | "2Og4zVhwsekYnFpP" | "Melissa Rinaldi" |
+    #| "2TlPS6XZ1eni0CMl" | "Cece Rosso"      |
 
--- src/test/java/serveRestStudy/stepdefinitions/User.java
+++ src/test/java/serveRestStudy/stepdefinitions/User.java
@@ -7,12 +7,12 @@ import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.testng.Assert.assertEquals;
 
 public class User {
@@ -55,5 +55,61 @@ public class User {
 
         List<Object> users = jsonPath.get("usuarios");
         Assert.assertFalse(users.isEmpty());
+    }
+
+    @Then("the userdata name should be displayed on the result list")
+    public void theUserdataNameShouldBeDisplayedOnTheResultList() {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.getJsonObject("usuarios[0].nome").toString();
+        Assert.assertFalse(userName.isEmpty());
+    }
+
+    @And("the userdata email should be displayed on the result list")
+    public void theUserdataEmailShouldBeDisplayedOnTheResultList() {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.getJsonObject("usuarios[0].email").toString();
+        Assert.assertFalse(userName.isEmpty());
+    }
+
+    @And("the userdata password should be displayed on the result list")
+    public void theUserdataPasswordShouldBeDisplayedOnTheResultList() {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.getJsonObject("usuarios[0].password").toString();
+        Assert.assertFalse(userName.isEmpty());
+    }
+
+    @And("the userdata administrator should be displayed on the result list")
+    public void theUserdataAdministratorShouldBeDisplayedOnTheResultList() {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.getJsonObject("usuarios[0].administrador").toString();
+        Assert.assertFalse(userName.isEmpty());
+    }
+
+    @And("the userdata _id should be displayed on the result list")
+    public void theUserdata_idShouldBeDisplayedOnTheResultList() {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.getJsonObject("usuarios[0]._id").toString();
+        Assert.assertFalse(userName.isEmpty());
+    }
+
+    @When("a userID {string} is passed on get users by ID endpoint")
+    public void aUserIDUserIDIsPassedOnGetUsersByIDEndpoint(String userID) {
+        RequestSpecification httpRequest = RestAssured.given();
+
+        response = httpRequest.get("usuarios/{_id}", userID);
+    }
+
+    @Then("the user {string} should be retrieved")
+    public void theUserUserShouldBeRetrieved(String user) {
+        JsonPath jsonPath = response.jsonPath();
+
+        String userName = jsonPath.get("nome");
+        assertEquals(userName, "Melissa Rinaldi");
+
     }
 }
